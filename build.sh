8d16c2bac861e517f4530b56dd85fe538b301cd0
Clay Gomera <maverick.xero@proton.me>
2024-08-07 17:29:26 -0400

    Fixed build script for windows

--- build.sh
+++ build.sh
@@ -5,10 +5,14 @@ if [[ -z "$package" ]]; then
     echo "usage: $0 <package-name>"
     exit 1
 fi
+
 package_split=(${package//\// })
 package_name=${package_split[-1]}
 
-platforms=("windows/amd64" "windows/arm64" "darwin/amd64" "darwin/arm64" "linux/amd64" "linux/arm64")
+buildir="$PWD/build"
+mkdir -p "$buildir"
+
+platforms=("windows/amd64" "darwin/amd64" "darwin/arm64" "linux/amd64" "linux/arm64")
 
 for platform in "${platforms[@]}"
 do
@@ -18,11 +22,16 @@ do
     output_name=$package_name'-'$GOOS'-'$GOARCH
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
+        CGO_ENABLED=1
+        CC=x86_64-w64-mingw32-gcc
+    else
+        CGO_ENABLED=0
+        CC=
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+    env GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=$CGO_ENABLED CC=$CC go build -o "$buildir"/$output_name $package
     if [ $? -ne 0 ]; then
-            echo 'An error has occurred! Aborting the script execution...'
+        echo 'An error has occurred! Aborting the script execution...'
         exit 1
     fi
 done

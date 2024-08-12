76ca325536f6e3ccc96f29253fd32c12f8faac8e
vhespanha <vhespanha@tuta.io>
2024-08-11 22:28:44 -0300

    Completion help boilerplate

--- cmd/completion.go
+++ cmd/completion.go
@@ -9,7 +9,45 @@ import (
 var completionCmd = &cobra.Command{
 	Use:   "completion [bash|zsh|fish|powershell]",
 	Short: "Generate completion script",
-	Long: `TODO`,
+	Long: `To load completions:
+
+Bash:
+
+  $ source <(clido completion bash)
+
+  # To load completions for each session, execute once:
+  # Linux:
+  $ clido completion bash > /etc/bash_completion.d/clido
+  # macOS:
+  $ clido completion bash > /usr/local/etc/bash_completion.d/clido
+
+Zsh:
+
+  # If shell completion is not already enabled in your environment,
+  # you will need to enable it.  You can execute the following once:
+
+  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+  # To load completions for each session, execute once:
+  $ clido completion zsh > "${fpath[1]}/_clido"
+
+  # You will need to start a new shell for this setup to take effect.
+
+fish:
+
+  $ clido completion fish | source
+
+  # To load completions for each session, execute once:
+  $ clido completion fish > ~/.config/fish/completions/clido.fish
+
+PowerShell:
+
+  PS> clido completion powershell | Out-String | Invoke-Expression
+
+  # To load completions for every new session, run:
+  PS> clido completion powershell > clido.ps1
+  # and source this file from your PowerShell profile.
+`,
 	DisableFlagsInUseLine: true,
 	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
 	Args:                  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),

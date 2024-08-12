7031489a1ba88324958aa929fb19233c6dabc469
vhespanha <vhespanha@tuta.io>
2024-08-11 22:24:13 -0300

    List tasks function (that was mostly chatgpt i admit)

--- cmd/list.go
+++ cmd/list.go
@@ -5,7 +5,10 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/d4r1us-drk/clido/pkg/models"
 	"github.com/d4r1us-drk/clido/pkg/repository"
+	"github.com/d4r1us-drk/clido/pkg/utils"
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
 
@@ -27,6 +30,9 @@ var listCmd = &cobra.Command{
 		defer repo.Close()
 
 		switch args[0] {
+		case "tasks":
+			projectFilter, _ := cmd.Flags().GetString("project")
+			listTasks(repo, projectFilter)
 		default:
 			fmt.Println("Invalid option. Use 'list projects' or 'list tasks'.")
 		}
@@ -36,4 +42,76 @@ var listCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(listCmd)
 	listCmd.Flags().StringP("project", "p", "", "Filter tasks by project name or ID")
+}
+
+func listTasks(repo *repository.Repository, projectFilter string) {
+	var tasks []*models.Task
+	var err error
+
+	if projectFilter != "" {
+		var project *models.Project
+		if utils.IsNumeric(projectFilter) {
+			projectID, _ := strconv.Atoi(projectFilter)
+			project, err = repo.GetProjectByID(projectID)
+		} else {
+			project, err = repo.GetProjectByName(projectFilter)
+		}
+
+		if err != nil || project == nil {
+			fmt.Printf("Project '%s' not found.\n", projectFilter)
+			return
+		}
+
+		tasks, err = repo.GetTasksByProjectID(project.ID)
+		if err != nil {
+			fmt.Printf("Error listing tasks: %v\n", err)
+			return
+		}
+
+		fmt.Printf("Tasks in project '%s':\n", project.Name)
+	} else {
+		tasks, err = repo.GetAllTasks()
+		if err != nil {
+			fmt.Printf("Error listing tasks: %v\n", err)
+			return
+		}
+
+		fmt.Println("All Tasks:")
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader(
+		[]string{
+			"ID",
+			"Name",
+			"Description",
+			"Due Date",
+			"Completed",
+			"Past Due",
+			"Priority",
+			"Project",
+		},
+	)
+	table.SetRowLine(true)
+
+	for _, task := range tasks {
+		project, _ := repo.GetProjectByID(task.ProjectID)
+		projectName := ""
+		if project != nil {
+			projectName = project.Name
+		}
+
+		table.Append([]string{
+			strconv.Itoa(task.ID),
+			utils.WrapText(task.Name, 20),
+			utils.WrapText(task.Description, 30),
+			utils.FormatDate(task.DueDate),
+			fmt.Sprintf("%v", task.TaskCompleted),
+			utils.ColoredPastDue(task.DueDate, task.TaskCompleted),
+			utils.GetPriorityString(task.Priority),
+			utils.WrapText(projectName, 20),
+		})
+	}
+
+	table.Render()
 }

064bd5a78d25ac02176a9119505b8e031a373769
vhespanha <vhespanha@tuta.io>
2024-08-11 21:44:53 -0300

    Move models to their own package

---
+++ pkg/models/task.go
@@ -0,0 +1,16 @@
+package models
+
+import "time"
+
+type Task struct {
+	ID              int
+	Name            string
+	Description     string
+	ProjectID       int
+	TaskCompleted   bool
+	DueDate         *time.Time
+	CompletionDate  *time.Time
+	CreationDate    time.Time
+	LastUpdatedDate time.Time
+	Priority        int
+}

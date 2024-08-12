ab34ee882e8403b8022e44855c783883584328c0
vhespanha <vhespanha@tuta.io>
2024-08-11 22:07:58 -0300

    Implement task model and createTask command

--- cmd/new.go
+++ cmd/new.go
@@ -5,7 +5,9 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/d4r1us-drk/clido/pkg/models"
 	"github.com/d4r1us-drk/clido/pkg/repository"
+	"github.com/d4r1us-drk/clido/pkg/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -27,9 +29,68 @@ var newCmd = &cobra.Command{
 		defer repo.Close()
 
 		switch args[0] {
+		case "task":
+			createTask(cmd, repo)
 		default:
 			fmt.Println("Invalid option. Use 'new project' or 'new task'.")
 		}
 	},
 }
 
+func createTask(cmd *cobra.Command, repo *repository.Repository) {
+	name, _ := cmd.Flags().GetString("name")
+	description, _ := cmd.Flags().GetString("description")
+	projectIdentifier, _ := cmd.Flags().GetString("project")
+	dueDateStr, _ := cmd.Flags().GetString("due")
+	priority, _ := cmd.Flags().GetInt("priority")
+
+	if name == "" || projectIdentifier == "" {
+		fmt.Println("Task name and project identifier are required.")
+		return
+	}
+
+	var project *models.Project
+	var err error
+
+	if utils.IsNumeric(projectIdentifier) {
+		projectID, _ := strconv.Atoi(projectIdentifier)
+		project, err = repo.GetProjectByID(projectID)
+	} else {
+		project, err = repo.GetProjectByName(projectIdentifier)
+	}
+
+	if err != nil || project == nil {
+		fmt.Printf("Project '%s' not found.\n", projectIdentifier)
+		return
+	}
+
+	var dueDate *time.Time
+	if dueDateStr != "" {
+		parsedDate, err := time.Parse("2006-01-02 15:04", dueDateStr)
+		if err == nil {
+			dueDate = &parsedDate
+		} else {
+			fmt.Println("Invalid date format. Using no due date.")
+		}
+	}
+
+	task := &models.Task{
+		Name:        name,
+		Description: description,
+		ProjectID:   project.ID,
+		DueDate:     dueDate,
+		Priority:    priority,
+	}
+
+	err = repo.CreateTask(task)
+	if err != nil {
+		fmt.Printf("Error creating task: %v\n", err)
+		return
+	}
+
+	fmt.Printf(
+		"Task '%s' created successfully with priority %s.\n",
+		name,
+		utils.GetPriorityString(priority),
+	)
+}

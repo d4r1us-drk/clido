592a2c16bfe7b37583f081e9a0f8021ac606b8f0
vhespanha <vhespanha@tuta.io>
2024-08-11 21:36:55 -0300

    Make main.go be cobra's entry point

--- main.go
+++ main.go
@@ -1,39 +1,7 @@
 package main
 
-import (
-	"os"
-
-	"github.com/fatih/color"
-)
+import "github.com/d4r1us-drk/clido/cmd"
 
 func main() {
-	if len(os.Args) < 2 {
-		handleHelp()
-		return
-	}
-
-	repo, err := NewRepository()
-	if err != nil {
-		color.Red("Error initializing repository: %v\n", err)
-		return
-	}
-	defer repo.Close()
-
-	switch os.Args[1] {
-	case "new":
-		handleNew(repo, os.Args[2:])
-	case "edit":
-		handleEdit(repo, os.Args[2:])
-	case "list":
-		handleList(repo, os.Args[2:])
-	case "remove":
-		handleRemove(repo, os.Args[2:])
-	case "toggle":
-		handleToggle(repo, os.Args[1:])
-	case "help":
-		handleHelp()
-	default:
-		color.Red("Invalid command.")
-		handleHelp()
-	}
+	cmd.Execute()
 }

a9e9ae45aa3eb08f9348f3dfcd83a63473ed856e
vhespanha <vhespanha@tuta.io>
2024-08-17 21:41:55 -0300

    Add gorm tags on models package

--- pkg/models/project.go
+++ pkg/models/project.go
@@ -3,10 +3,13 @@ package models
 import "time"
 
 type Project struct {
-	ID               int       `json:"id"`
-	Name             string    `json:"name"`
+	ID               int       `gorm:"primaryKey" json:"id"`
+	Name             string    `gorm:"unique;not null" json:"name"`
 	Description      string    `json:"description"`
-	CreationDate     time.Time `json:"creation_date"`
-	LastModifiedDate time.Time `json:"last_modified_date"`
+	CreationDate     time.Time `gorm:"not null" json:"creation_date"`
+	LastModifiedDate time.Time `gorm:"not null" json:"last_modified_date"`
 	ParentProjectID  *int      `json:"parent_project_id,omitempty"`
+	ParentProject    *Project  `gorm:"foreignKey:ParentProjectID" json:"-"`
+	SubProjects      []Project `gorm:"foreignKey:ParentProjectID" json:"-"`
+	Tasks            []Task    `gorm:"foreignKey:ProjectID" json:"-"`
 }
